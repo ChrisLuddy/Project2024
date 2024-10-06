b216070f830eafe6c1aab2a58a079a6cdfbac2b8
vladar21 <vladyslav.rastvorov@mycit.ie>
2024-10-06 22:10:17 +0100

    feat: create core app for main business logic

--- backend/act_backend/settings.py
+++ backend/act_backend/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',  # work with API
+    'core',  # main business logic
 ]
 
 MIDDLEWARE = [
